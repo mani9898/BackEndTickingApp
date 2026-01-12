b2f9b129081ecd34f02de9cd52d668d6d8b12463
Manpreet <contact@manpreetsingh.dev>
2026-01-12 10:28:17 -0500

    first commit base

---
+++ BackEndTickingApp/src/main/java/org/example/backendtickingapp/BackEndTickingAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendtickingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTickingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndTickingAppApplication.class, args);
+    }
+
+}
---
+++ BackEndTickingApp/src/test/java/org/example/backendtickingapp/BackEndTickingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendtickingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTickingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
